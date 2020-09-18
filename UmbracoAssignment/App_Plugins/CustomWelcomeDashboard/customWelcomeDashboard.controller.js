d124bce56c3db5c327adcb9120d14b5ce7e38564
Erika Fröjd <erika.frojd1@learnet.se>
2020-09-18 11:01:41 +0200

    Add project files.

---
+++ UmbracoAssignment/App_Plugins/CustomWelcomeDashboard/customWelcomeDashboard.controller.js
@@ -0,0 +1,10 @@
+﻿angular.module("umbraco").controller("CustomWelcomeDashboardController", function ($scope, userService) {
+    var vm = this;
+    vm.UserName = "guest";
+    var user = userService.getCurrentUser().then(function (user) {
+        console.log(user);
+        vm.UserName = user.name;
+    });
+});
+
+
---
+++ UmbracoAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UmbracoAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UmbracoAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76a6d52c-8bf6-4c48-ba1c-cfcbd6d0658e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
